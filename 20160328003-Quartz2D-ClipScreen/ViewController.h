db86b4687d944d6017e681bd36fda378ef0e247e
raineryue <it_jinlei.yue@163.com>
2016-03-28 15:46:50 +0800

    Initial Commit

---
+++ 20160328003-Quartz2D-ClipScreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160328003-Quartz2D-ClipScreen
+//
+//  Created by Rainer on 16/3/28.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
